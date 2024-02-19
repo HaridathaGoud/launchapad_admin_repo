0b6c4df7bc200028d3e6dad6a88ee09356455eb6
subbarao <subbarao@yellowblock.net>
2024-02-19 09:57:27 +0530

    Edit-cast_crew_Selected-roles_binded

--- src/components/launchpad/projects/projects.js
+++ src/components/launchpad/projects/projects.js
@@ -198,7 +198,7 @@ const Projects = (props) => {
     dispatch({ type: 'castImgLoader', payload: false })
     if (index !== null) {
     const selectedItem = state.castCrewDataList[index];
-    setSelectedroleValues(selectedItem?.role)
+    setSelectedroleValues(selectedItem?.role?.map(role => ({ role })));
     dispatch({ type: "cast_CrewsFormDeatils" ,payload: selectedItem });
     }else{
       dispatch({ type: "cast_CrewsFormDeatils", payload: {} });
@@ -543,13 +543,10 @@ const Projects = (props) => {
     setSelectedroleValues(selectedList);
     dispatch({ type: 'cast_CrewsFormDeatils',payload: { ...state.cast_CrewsFormDeatils, role: selectedRoleNames } })
   };
-const validateCastCrewForm=()=>{
+ const validateCastCrewForm=()=>{
   let validatingForm = {...state.cast_CrewsFormDeatils}
-  const newErrors = {};// state.cast_CrewsFormDeatils
-  const emojiRejex =
-    /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff]|[\u2010-\u2017])/g;
-    const urlRegex = /^www\.[a-zA-Z]+\.(com)$/;
-
+  const newErrors = {};
+  const urlRegex = /^www\.[a-zA-Z]+\.(com)$/;
     if (!validatingForm?.name || validatingForm?.name === '') {
     newErrors.name = 'Is required';
   } else if (!validateContentRules('', validatingForm?.name)) {
