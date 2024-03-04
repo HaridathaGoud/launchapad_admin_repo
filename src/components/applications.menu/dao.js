79310e721ecb1354427a5af28b25622f08fb5169
subbarao <subbarao@yellowblock.net>
2024-03-04 11:50:19 +0530

    daoMenu

--- src/components/applications.menu/dao.js
+++ src/components/applications.menu/dao.js
@@ -2,21 +2,11 @@ import React from 'react'
 import { CNavItem, CNavLink } from '@coreui/react'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
-import { useSelector ,useDispatch} from "react-redux"
-import { setApp } from 'src/reducers/applicationReducer';
+import { useSelector} from 'react-redux';
 
 const DaoMenu = (props) => {
     const isAdmin = useSelector(state => state.oidc?.adminDetails);
-    const app_name = useSelector(state => state.application.app_name);
-    const dispatch = useDispatch()
-    const onAppSelect = (app_name) => {
-        dispatch(setApp(app_name));
-      }
-      const handleMenuNavigateItem = (menuItem, isSettings, app_name) => {
-        if (app_name) {
-          onAppSelect(app_name);
-        }
-    }
+
     const renderTooltipDashboard = (props) => (
         <Tooltip id="button-tooltip" {...props}> 
             Dashboard
@@ -27,16 +17,6 @@ const DaoMenu = (props) => {
             Customers
         </Tooltip>
     );
-    // const logoRedirection=()=>{
-    //     if (isAdmin?.isAdmin) {
-    //       onAppSelect("launchpad");
-    //       navigate('/launchpad/customers');
-    //     }else{
-    //        onAppSelect("launchpad");
-    //       navigate('/launchpad/dashboard');
-    //     }
-    
-    //   }
     const { handleMenuNavigate } = props;
     let locationSplit = location?.pathname?.split('/');
     if (isAdmin?.isAdmin) { 
