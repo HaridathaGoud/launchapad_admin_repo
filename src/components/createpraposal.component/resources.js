918b089c2eb2a70b1b271818ff9587c6ed2398b1
subbarao <subbarao@yellowblock.net>
2024-03-04 15:12:46 +0530

    Resources file removed empty tags

--- src/components/createpraposal.component/resources.js
+++ src/components/createpraposal.component/resources.js
@@ -2,10 +2,9 @@ import React from 'react';
 import resource from '../../assets/images/resource.png'
 export default function Resources() {
     return (
-        <>
                         <div className='voting-card resources mt-5'>
                             <div className='d-flex justify-content-between align-items-center px-3 py-2'>
-                                <img src={resource}></img>
+                                <img src={resource} alt=''></img>
                                 <h1 className='resource-title mb-0 me-5'>No resources were added</h1>
                             </div><hr className='m-0'/>
                             <div className=' px-4 py-2'>
@@ -22,6 +21,5 @@ export default function Resources() {
                                 </p></div>
                             </div>
                         </div>
-        </>
     );
 }
